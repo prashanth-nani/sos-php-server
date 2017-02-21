1ef40e59f7f719367baf01ab96815ccae7104d36
Prashanth <prashanth1395@gmail.com>
2017-02-21 13:02:23 +0530

    Add login and register validation

--- login.php
+++ login.php
@@ -3,8 +3,9 @@
 
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       $email = mysqli_real_escape_string($db,$_POST['email']);
+      $password = mysqli_real_escape_string($db,$_POST['password']);
 
-      $sql = "SELECT id FROM user WHERE email = '$email'";
+      $sql = "SELECT id FROM user WHERE email = '$email' and password = '$password'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
 
--- register.php
+++ register.php
@@ -1,5 +1,29 @@
 <?php
+include("config.php");
+
 $name = $_POST['name'];
+$email = $_POST['email'];
+$gender = $_POST['gender'];
+$phone = $_POST['phone'];
 $password =  $_POST['password'];
-echo "Your name is ".$name." and your password is ".$password;
+
+if(! $db ) {
+      echo "FAILED";
+      die('Could not connect: ' . mysql_error());
+   }
+
+  //  $stmt = $mysqli->prepare('INSERT INTO user (name, phone, gender, email, password) VALUES ( ?, ?, ?, ?, ? )');
+   //
+  //  $stmt->bind_param("sisss", $name, $phone, $gender, $email, $password);
+  //  $stmt->execute();
+  //  $stmt->close();
+  $regquery = "INSERT INTO user (name, phone, gender, email, password) VALUES('$name', '$phone', '$gender', '$email', '$password')";
+  $regresult = mysqli_query($db, $regquery);
+  if($regresult == 1){
+    echo "SUCCESS";
+  }
+  else{
+    echo "FAILED";
+  }
+
  ?>
